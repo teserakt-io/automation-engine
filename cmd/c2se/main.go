6b619c5dec76b2e97cc0d534306d9a72dc0ced8e
JP Aumasson <jeanphilippe.aumasson@gmail.com>
2019-04-02 18:16:44 +0200

    short gitcommit from build script

--- cmd/c2se/main.go
+++ cmd/c2se/main.go
@@ -9,11 +9,11 @@ var buildDate string
 func main() {
 
 	if len(gitTag) == 0 {
-		fmt.Printf("E4: C2 scripting engine - version %s-%s\n", buildDate, gitCommit[:4])
+		fmt.Printf("E4: C2 scripting engine - version %s-%s\n", buildDate, gitCommit)
 	} else {
-		fmt.Printf("E4: C2 scripting engine - version %s (%s-%s)\n", gitTag, buildDate, gitCommit[:4])
+		fmt.Printf("E4: C2 scripting engine - version %s (%s-%s)\n", gitTag, buildDate, gitCommit)
 	}
-	fmt.Println("Copyright (c) Teserakt AG, 2018-2019")
+	fmt.Println("Copyright (c) Teserakt AG, 2018-2019. All rights reserved.")
 
 	// TODO: everything.
 }
--- cmd/c2sr/main.go
+++ cmd/c2sr/main.go
@@ -199,11 +199,11 @@ func main() {
 	db.LogMode(false)
 
 	if len(gitTag) == 0 {
-		fmt.Printf("E4: C2 script reader - version %s-%s\n", buildDate, gitCommit[:4])
+		fmt.Printf("E4: C2 script reader - version %s-%s\n", buildDate, gitCommit)
 	} else {
-		fmt.Printf("E4: C2 script reader - version %s (%s-%s)\n", gitTag, buildDate, gitCommit[:4])
+		fmt.Printf("E4: C2 script reader - version %s (%s-%s)\n", gitTag, buildDate, gitCommit)
 	}
-	fmt.Println("Copyright (c) Teserakt AG, 2018-2019")
+	fmt.Println("Copyright (c) Teserakt AG, 2018-2019. All rights reserved.")
 
 	for _, fileName := range fileNames {
 		rulesClient, rulesTopic, err := parseRules(fileName)
