6fbf94ac53f8b1504b6c62f66399846d6846a7dc
Flavien binet <flavien.binet@gmail.com>
2019-04-10 10:57:04 +0200

    Fixed build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-PROJECT=c2sr
+PROJECT=c2se
 
-echo "$PROJECT build script (c) Teserakt AG 2018. All rights reserved."
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "$PROJECT build script (c) Teserakt AG 2018-2019. All rights reserved."
 echo ""
 
-goimports -w cmd/$PROJECT
-
 GIT_COMMIT=$(git rev-list -1 HEAD)
-GIT_TAG=$(git describe --exact-match HEAD)
+GIT_TAG=$(git describe --exact-match HEAD 2>/dev/null)
 NOW=$(date "+%Y%m%d")
 
 GOOS=`uname -s | tr '[:upper:]' '[:lower:]'` 
 GOARCH=amd64
 
-printf "building $PROJECT:\n\tversion $NOW-$GIT_COMMIT\n\tOS $GOOS\n\tarch: $GOARCH\n"
+CMDS=($(find ${DIR}/../cmd/ -mindepth 1 -maxdepth 1  -type d -exec basename {} \;))
+for cmd in ${CMDS[@]}; do
 
-printf "=> $PROJECT...\n"
-GOOS=$GOOS GOARCH=$GOARCH go build -o bin/$PROJECT -ldflags "-X main.gitTag=$GIT_TAG -X main.gitCommit=$GIT_COMMIT -X main.buildDate=$NOW" ${PWD}/cmd/$PROJECT
+    printf "building ${PROJECT}-${cmd}:\n\tversion ${NOW}-${GIT_COMMIT}\n\tOS ${GOOS}\n\tarch: ${GOARCH}\n"
+
+    printf "=> ${PROJECT}-${cmd}...\n"
+    GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${DIR}/../bin/${PROJECT}-${cmd} -ldflags "-X main.gitTag=${GIT_TAG} -X main.gitCommit=${GIT_COMMIT} -X main.buildDate=${NOW}" ${DIR}/../cmd/${cmd}
+done
--- scripts/buildproto.sh
+++ scripts/buildproto.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 protoc --proto_path ${DIR}/../ api.proto --go_out=plugins=grpc:${DIR}/../internal/pb
