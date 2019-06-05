f76935e5dfcc4a92f89001cb5710b653b78c10bd
Flavien binet <flavien.binet@gmail.com>
2019-06-05 16:53:13 +0200

    Added docker image build and CI for C2AE api and cli

---
+++ scripts/docker-build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "c2ae Docker build script (c) Teserakt AG 2018-2019. All Right Reserved"
+echo ""
+
+# docker buikd --ssh require at least docker 18.09
+if [ -z "$(docker --version | grep 18.09)" ]; then
+    echo "docker >= 18.09 is required"
+    exit 1
+fi
+
+# A running ssh-agent is required to forward the connection to the docker build process.
+# This allow to reuse host ssh / git configuration to clone private repositories from gitlab.
+if [ -z "$SSH_AUTH_SOCK" ]; then
+    echo "SSH_AUTH_SOCK is required to run the docker build"
+    exit 1
+else
+    echo "Using SSH_AUTH_SOCK=$SSH_AUTH_SOCK"
+fi
+
+E4_VERSION="${BUILDVERSION}"
+E4_GIT_COMMIT=$(git rev-list -1 HEAD)
+
+if [[ -z "$E4_VERSION" ]]; then
+    E4_VERSION="devel"
+fi
+
+echo "Building version $E4_VERSION, commit $E4_GIT_COMMIT\n"
+
+printf "=> c2ae-api"
+DOCKER_BUILDKIT=1 docker build \
+    --ssh default \
+    --target c2ae-api \
+    --tag "registry.gitlab.com/teserakt/c2ae/api:$E4_VERSION" \
+    --tag "registry.gitlab.com/teserakt/c2ae/api:$E4_GIT_COMMIT" \
+    -f "${DIR}/../docker/c2ae/Dockerfile" \
+    "${DIR}/../"
+
+printf "=> c2ae-cli"
+DOCKER_BUILDKIT=1 docker build \
+    --ssh default \
+    --target c2ae-cli \
+    --tag "registry.gitlab.com/teserakt/c2ae/cli:$E4_VERSION" \
+    --tag "registry.gitlab.com/teserakt/c2ae/cli:$E4_GIT_COMMIT" \
+    -f "${DIR}/../docker/c2ae/Dockerfile" \
+    "${DIR}/../"
